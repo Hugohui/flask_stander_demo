b0d0ad9236d4f9afe536aed92490fdee5e81d2e0
huiwenhua <huiwenhua@liquidnetwork.com>
2019-09-17 20:04:27 +0800

    docker

--- settings.py
+++ settings.py
@@ -6,14 +6,14 @@
 :date: 2019-08-27
 """
 
-IS_DEBUG = True
+IS_DEBUG = False
 PORT = 5203
 
 # 是否开启扫码登陆
-FORCE_LOGIN = True
+FORCE_LOGIN = False
 
 # MongoDB
-MONGO_HOST = "127.0.0.1" if IS_DEBUG else ""
+MONGO_HOST = "127.0.0.1" if IS_DEBUG else "mongodb://admin:n3tw0rk@dds-2ze4b601d1f5a9141374-pub.mongodb.rds.aliyuncs.com:3717,dds-2ze4b601d1f5a9142107-pub.mongodb.rds.aliyuncs.com:3717/ab_test?replicaSet=mgset-16573833"
 
 # 扫码登陆用户MongoDB
 USER_MONGO_HOST = "mongodb://liquid:n3tw0rk@dds-2ze5fffe69c1c5541874-pub.mongodb.rds.aliyuncs.com:3717,dds-2ze5fffe69c1c5542283-pub.mongodb.rds.aliyuncs.com:3717/activity?replicaSet=mgset-13269097" if IS_DEBUG else "mongodb://liquid:n3tw0rk@dds-2ze5fffe69c1c5541.mongodb.rds.aliyuncs.com:3717,dds-2ze5fffe69c1c5542.mongodb.rds.aliyuncs.com:3717/activity?replicaSet=mgset-13269097"
