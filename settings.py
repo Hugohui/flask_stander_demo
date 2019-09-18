f33277fd576759f903135d2b612468580742e0cd
huiwenhua <huiwenhua@liquidnetwork.com>
2019-09-18 21:05:15 +0800

    扫码登录

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ IS_DEBUG = False
 PORT = 5203
 
 # 是否开启扫码登陆
-FORCE_LOGIN = False
+FORCE_LOGIN = True
 
 # MongoDB
 # 外网 "mongodb://admin:n3tw0rk@dds-2ze4b601d1f5a9141374-pub.mongodb.rds.aliyuncs.com:3717,dds-2ze4b601d1f5a9142107-pub.mongodb.rds.aliyuncs.com:3717/ab_test?replicaSet=mgset-16573833"
@@ -22,7 +22,7 @@ USER_MONGO_HOST = "mongodb://liquid:n3tw0rk@dds-2ze5fffe69c1c5541874-pub.mongodb
 
 # 扫码登录
 # 项目访问地址
-DINGDING_SRC = "http://10.0.100.76:8081" if IS_DEBUG else "http://xxx.liquidnetwork.com"
+DINGDING_SRC = "http://10.0.100.76:8080" if IS_DEBUG else "http://liquid-h5.oss-cn-beijing.aliyuncs.com/ab_test/index.html#/index"
 
 # 分配的key
 DINGDING_KEY = "ABTest-devFhWCJi32po56HvoKJ"
