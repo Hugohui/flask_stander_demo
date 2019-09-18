135db7205a461c4c1913ee76169bfedbe7243b82
huiwenhua <huiwenhua@liquidnetwork.com>
2019-09-18 15:06:35 +0800

    mongo

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ FORCE_LOGIN = False
 # MongoDB
 # 外网 "mongodb://admin:n3tw0rk@dds-2ze4b601d1f5a9141374-pub.mongodb.rds.aliyuncs.com:3717,dds-2ze4b601d1f5a9142107-pub.mongodb.rds.aliyuncs.com:3717/ab_test?replicaSet=mgset-16573833"
 # 内网 "mongodb://root:n3tw0rk@dds-2ze4b601d1f5a9141.mongodb.rds.aliyuncs.com:3717,dds-2ze4b601d1f5a9142.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-16573833"
-MONGO_HOST = "127.0.0.1" if IS_DEBUG else "mongodb://admin:n3tw0rk@dds-2ze4b601d1f5a9141.mongodb.rds.aliyuncs.com:3717,dds-2ze4b601d1f5a9142.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-16573833"
+MONGO_HOST = "127.0.0.1" if IS_DEBUG else "mongodb://admin:n3tw0rk@dds-2ze4b601d1f5a9141.mongodb.rds.aliyuncs.com:3717,dds-2ze4b601d1f5a9142.mongodb.rds.aliyuncs.com:3717/ab_test?replicaSet=mgset-16573833"
 
 # 扫码登陆用户MongoDB
 USER_MONGO_HOST = "mongodb://liquid:n3tw0rk@dds-2ze5fffe69c1c5541874-pub.mongodb.rds.aliyuncs.com:3717,dds-2ze5fffe69c1c5542283-pub.mongodb.rds.aliyuncs.com:3717/activity?replicaSet=mgset-13269097" if IS_DEBUG else "mongodb://liquid:n3tw0rk@dds-2ze5fffe69c1c5541.mongodb.rds.aliyuncs.com:3717,dds-2ze5fffe69c1c5542.mongodb.rds.aliyuncs.com:3717/activity?replicaSet=mgset-13269097"
