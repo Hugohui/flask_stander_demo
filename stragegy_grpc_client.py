c5c1779339094f5c9efd85fdb3c07ac24f70d05a
huiwenhua <huiwenhua@liquidnetwork.com>
2019-09-19 19:55:30 +0800

    redis

--- stragegy_grpc_client.py
+++ stragegy_grpc_client.py
@@ -11,11 +11,11 @@ import stragegy_pb2_grpc
 
 def run():
     # 连接 rpc 服务器
-    channel = grpc.insecure_channel('localhost:50051')
-    # channel = grpc.insecure_channel('47.93.218.129:50051')
+    # channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('47.93.218.129:50051')
     # 调用 rpc 服务
     stub = stragegy_pb2_grpc.StragegyStub(channel)
-    response = stub.GetStragegy(stragegy_pb2.GetRequest(md5_id='rC8epZTEcw7cSykzvqftiS5iEiE', test_id="e293f5ae-cef8-11e9-a429-c06e964480ff"))
+    response = stub.GetStragegy(stragegy_pb2.GetRequest(md5_id='rC8epZTEcw7cSykzvqftiS5iEiE', test_id="28de117a-dad4-11e9-bfd2-0242ac120003"))
     print("Greeter client received: " + response.stragegy_id)
 
 if __name__ == '__main__':
