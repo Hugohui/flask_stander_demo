86a5f6f103d1bcee89ebf1791aa82026b97f1fea
huiwenhua <huiwenhua@liquidnetwork.com>
2019-11-01 15:53:44 +0800

    修改扫码登录

--- settings.py
+++ settings.py
@@ -33,5 +33,7 @@ DINGDING_SRC = "http://10.0.100.76:8080" if IS_DEBUG else "http://liquid-h5.oss-
 # 分配的key
 DINGDING_KEY = "ABTest-devFhWCJi32po56HvoKJ"
 
-# 权限后台地址
-IF_PERMISSION_URL = "http://scancode.liquidnetwork.com"
+# 权限后台地址ECS部署
+# IF_PERMISSION_URL = "http://scancode.liquidnetwork.com"
+# 权限后台地址k8s部署
+IF_PERMISSION_URL = "http://base-login:80"
--- stragegy_grpc_client.py
+++ stragegy_grpc_client.py
@@ -11,8 +11,8 @@ import stragegy_pb2_grpc
 
 def run():
     # 连接 rpc 服务器
-    channel = grpc.insecure_channel('localhost:50051')
-    # channel = grpc.insecure_channel('47.93.218.129:50051')
+    channel = grpc.insecure_channel('ab-test-grpc:50050')
+    # channel = grpc.insecure_channel('112.126.118.70:50050')
     # 调用 rpc 服务
     stub = stragegy_pb2_grpc.StragegyStub(channel)
     response = stub.GetStragegy(stragegy_pb2.GetRequest(md5_id='rC8epZTEcw7cSykzvqftiS5iEiE', test_id="e293f5ae-cef8-11e9-a429-c06e964480ff"))
