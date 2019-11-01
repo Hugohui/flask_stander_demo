309a58d530449df3c28c82d34c2f9b04a1caba69
huiwenhua <huiwenhua@liquidnetwork.com>
2019-11-01 17:26:22 +0800

    grpc测试通过

--- stragegy_grpc_client.py
+++ stragegy_grpc_client.py
@@ -11,11 +11,11 @@ import stragegy_pb2_grpc
 
 def run():
     # 连接 rpc 服务器
-    channel = grpc.insecure_channel('ab-test-grpc:50050')
-    # channel = grpc.insecure_channel('112.126.118.70:50050')
+    # channel = grpc.insecure_channel('ab-test-grpc:50050')
+    channel = grpc.insecure_channel('112.126.118.70:50050')
     # 调用 rpc 服务
     stub = stragegy_pb2_grpc.StragegyStub(channel)
-    response = stub.GetStragegy(stragegy_pb2.GetRequest(md5_id='rC8epZTEcw7cSykzvqftiS5iEiE', test_id="e293f5ae-cef8-11e9-a429-c06e964480ff"))
+    response = stub.GetStragegy(stragegy_pb2.GetRequest(md5_id='rC8epZTEcw7cSykzvqftiS5iEiE', test_id="28de117a-dad4-11e9-bfd2-0242ac120003"))
     print("Greeter client received result: " + response.result)
 
 if __name__ == '__main__':
