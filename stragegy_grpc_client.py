6abad33c498351de3e8c0af56afc9ebd35431121
huiwenhua <huiwenhua@liquidnetwork.com>
2019-11-12 15:14:06 +0800

    utils修改

--- stragegy_grpc_client.py
+++ stragegy_grpc_client.py
@@ -5,7 +5,7 @@
 :date: 2019-09-10
 """
 
-import grpc
+import grpc, random
 import stragegy_pb2
 import stragegy_pb2_grpc
 
@@ -15,7 +15,7 @@ def run():
     channel = grpc.insecure_channel('112.126.118.70:50050')
     # 调用 rpc 服务
     stub = stragegy_pb2_grpc.StragegyStub(channel)
-    response = stub.GetStragegy(stragegy_pb2.GetRequest(md5_id='cSykzvEEEqaaaaftiS5is9', test_id="70c46782-01d5-11ea-ab13-c63dfbc32fe9"))
+    response = stub.GetStragegy(stragegy_pb2.GetRequest(md5_id=str(random.randint(0,10000)), test_id="70c46782-01d5-11ea-ab13-c63dfbc32fe9"))
     print("Greeter client received result: " + response.result)
 
 if __name__ == '__main__':
